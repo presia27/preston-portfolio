8d25896cfec9deca02fbe4d433c360c191f02392
presia27 <pre.sia977@outlook.com>
2026-01-05 00:43:02 -0800

    Home page additions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
-
-
 export default function Home() {
   return (
     <main className="contentMargins mt-16">
       <section>
-        <div>
-          <h1>Welcome!</h1>
-          <p>
-            I&apos;m Preston, a software developer in the Seattle area.
-          </p>
+        <div className="flex flex-row justify-between">
+          <div>
+            <h1 className="bigHeader">Welcome!</h1>
+            <p>
+              I&apos;m Preston, a software developer in the Seattle area.
+            </p>
+          </div>
+
+          <div className="border-l border-accent-darker px-10 py-4 mr-[25%]">
+            <h2 className="font-bold border-b border-accent-darker">Preston Sia</h2>
+            <ul>
+              <li>Email: <a href="mailto:pre.sia977@outlook.com" className="text-cyan-900 underline">pre.sia977@outlook.com</a></li>
+              <li>GitHub: <a href="https://github.com/presia27" className="text-cyan-900 underline">presia27</a></li>
+              <li>LinkedIn: <a href="https://www.linkedin.com/in/preston-sia" className="text-cyan-900 underline">Preston Sia</a></li>
+            </ul>
+          </div>
         </div>
+      </section>
+      <section>
+        <h2 className="bigHeader">Featured</h2>
       </section>
     </main>
   );
---
+++ src/data/projects.ts
@@ -0,0 +1,23 @@
+const projects = [
+  {
+    title: ''
+  },
+  {
+    id: 8,
+    title: 'Is The Bridge Up?',
+    role: 'Full Stack Web Developer',
+    association: 'Personal Project',
+    client: null,
+    stack: ['TypeScript', 'React', 'Node.js', 'Express.js'],
+    startDate: '2025-03-19',
+    endDate: null,
+    projectPage: 'https://github.com/presia27/IsTheBridgeUp',
+    liveSite: 'https://isthebridgeup.vercel.app',
+    synopsis: 'Displays the status of Seattle\'s movable highway bridges',
+    longDescription: 'A full-stack project that shows when Seattle\'s moveable bridges are up for boat traffic.Users can view traffic camera images or view a summary of all bridges during their commute.',
+    contributions: [
+      'Developed a full-stack web application to show real-time data on Seattle\'s movable bridge openings.',
+      'Designed a lightweight database for storing bridge and traffic image data, deployed the app to Vercel and the database to Supabase to practice cloud deployment workflows'
+    ]
+  }
+];
